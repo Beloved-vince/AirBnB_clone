28d9f4f3085e47bd80c5533266c75c92147b6b3e
Oluwafemi <ogundarearnold@gmail.com>
2022-11-22 09:01:02 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,77 @@
 #!/usr/bin/python3
-"""This module contains the BaseModel class"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
 class BaseModel():
-    """This class will define all common attributes/methods for other classes"""
-    def __init__(self, *args, **kwargs):
-        """Instantiation of base model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+    id = uuid4()
+
+    def __init__(self, int=None):
+        self.my_number = int
+        self.name = str
+        self.updated_at = datetime.now()
+        self.id = self.id
+        self.created_at = datetime.now()
 
     def __str__(self):
-        """Overriding the __str__ method"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,self.__dict__)
+        """
+        Return the following objects
+        Args:
+            class name:
+                in format [BaseModel]
+            id:
+                to hold the unique class identifier
+            created_at:
+                        date object is created
+            updated_at:
+                        date object is updated
+        """
+
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        """Updates the public instance attribute updated_at"""
+        """
+        save update_at anytime this function is call
+        """
+
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__"""
-        new_dict = dict(self.__dict__)
-        new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
-        return new_dict
+        """
+        Return a dictionary containing key and value of the instance\
+                including class name
+        """
+
+        dic = {}
+        for key, value in self.__dict__.items():
+            dic[key] = value
+            if key in ["created_at", "updated_at"]:
+                dic['created_at'] = self.created_at.isoformat()
+                dic['updated_at'] = self.updated_at.isoformat()
+                
+        dic['__class__'] = self.__class__.__name__
+
+        return dic
     
-    
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    if __name__ == "__main__":
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        print(my_model.id)
+        print(my_model)
+        print(type(my_model.created_at))
+        print("--")
+        my_model_json = my_model.to_dict()
+        print(my_model_json)
+        print("JSON of my_model:")
+        for key in my_model_json.keys():
+            print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+        print("--")
+        my_new_model = BaseModel(**my_model_json)
+        print(my_new_model.id)
+        print(my_new_model)
+        print(type(my_new_model.created_at))
+
+        print("--")
+        print(my_model is my_new_model)

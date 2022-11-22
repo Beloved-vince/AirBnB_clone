ed86957225b4b2f25d28936e5dcd162fb49b3572
Oluwafemi <ogundarearnold@gmail.com>
2022-11-22 07:18:35 +0100

    Authors and Console refactored

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module contains the init file from models"""
+"""This module contains the init file for models"""
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """This module contains the BaseModel class"""
 import uuid
 from datetime import datetime
 import models
+
 class BaseModel:
     """This class defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
@@ -32,6 +32,12 @@ class BaseModel:
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
+    
+    def delete(self):
+        """Delete the current instance from the storage (models.storage)"""
+        models.storage.delete(self)
+    
+
 if __name__ == "__main__":
     my_model = BaseModel()
     my_model.name = "Holberton"
@@ -48,57 +54,3 @@ if __name__ == "__main__":
     print(my_model.updated_at)
     my_model.save()
     
-=======
-from uuid import uuid4
-from datetime import datetime
-
-class BaseModel():
-    id = uuid4()
-
-    def __init__(self, int=None):
-        self.my_number = int
-        self.name = str
-        self.updated_at = datetime.now()
-        self.id = self.id
-        self.created_at = datetime.now()
-
-    def __str__(self):
-        """
-        Return the following objects
-        Args:
-            class name:
-                in format [BaseModel]
-            id:
-                to hold the unique class identifier
-            created_at:
-                        date object is created
-            updated_at:
-                        date object is updated
-        """
-
-        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
-
-    def save(self):
-        """
-        save update_at anytime this function is call
-        """
-
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """
-        Return a dictionary containing key and value of the instance\
-                including class name
-        """
-
-        dic = {}
-        for key, value in self.__dict__.items():
-            dic[key] = value
-            if key in ["created_at", "updated_at"]:
-                dic['created_at'] = self.created_at.isoformat()
-                dic['updated_at'] = self.updated_at.isoformat()
-                
-        dic['__class__'] = self.__class__.__name__
-
-        return dic
->>>>>>> a6d5d8a45b68386b917671b836ac99feb522a187

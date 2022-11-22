27eb86266f69458c9a8bddb347141bdf0d8b3136
Oluwafemi <ogundarearnold@gmail.com>
2022-11-22 09:11:56 +0100

    Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
-import models
 
 class BaseModel():
     id = uuid4()
@@ -53,25 +52,18 @@ class BaseModel():
 
         return dic
     
-    if __name__ == "__main__":
-        my_model = BaseModel()
-        my_model.name = "My_First_Model"
-        my_model.my_number = 89
-        print(my_model.id)
-        print(my_model)
-        print(type(my_model.created_at))
-        print("--")
-        my_model_json = my_model.to_dict()
-        print(my_model_json)
-        print("JSON of my_model:")
-        for key in my_model_json.keys():
-            print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
-        print("--")
-        my_new_model = BaseModel(**my_model_json)
-        print(my_new_model.id)
-        print(my_new_model)
-        print(type(my_new_model.created_at))
-
-        print("--")
-        print(my_model is my_new_model)
+    

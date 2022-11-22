80fac3edcd61355a2d4859352168681bc301b078
Oluwafemi <ogundarearnold@gmail.com>
2022-11-22 08:41:24 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,8 @@ import uuid
 from datetime import datetime
 import models
 
-class BaseModel:
-    """This class defines all common attributes/methods for other classes"""
+class BaseModel():
+    """This class will define all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """Instantiation of base model"""
         if kwargs:
@@ -18,39 +18,34 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
+
     def __str__(self):
-        """String representation of BaseModel"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Overriding the __str__ method"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,self.__dict__)
+
     def save(self):
-        """Updates the public instance attribute updated_at with the current datetime"""
+        """Updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
         models.storage.save()
+
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
-        new_dict = self.__dict__.copy()
+        """Returns a dictionary containing all keys/values of __dict__"""
+        new_dict = dict(self.__dict__)
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
     
-    def delete(self):
-        """Delete the current instance from the storage (models.storage)"""
-        models.storage.delete(self)
     
-
 if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "Holberton"
+    my_model.name = "My First Model"
     my_model.my_number = 89
     print(my_model)
-    print(my_model.__dict__)
-    print(my_model.to_dict())
-    print(type(my_model.created_at))
-    print(type(my_model.updated_at))
-    print(type(my_model.to_dict()["created_at"]))
-    print(type(my_model.to_dict()["updated_at"]))
-    print(my_model.id)
-    print(my_model.created_at) 
-    print(my_model.updated_at)
     my_model.save()
-    
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))

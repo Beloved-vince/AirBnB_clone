8e73b54cdec3966db4db5ca64c9c1e76c59e0e35
Beloved-vince <vinceoludare@gmail.com>
2022-11-26 15:31:30 +0100

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,9 @@ BaseModel class is created to generate a dictionary representation\
         of an instance
 On this class other classes will inherit from
 """
-
 from uuid import uuid4
 from datetime import datetime
 import models
-
 format_dt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
@@ -48,15 +46,10 @@ class BaseModel():
     def __str__(self):
         """
         Return the following objects
-        Args:
-            class name:
-                in format [BaseModel]
-            id:
-                to hold the unique class identifier
-            created_at:
-                        date object is created
-            updated_at:
-                        date object is updated
+            class name: in format [BaseModel]
+            id: to hold the unique class identifier
+            created_at: date object is created
+            updated_at: date object is updated
         """
         class_name = self.__class__.__name__
         uuid = self.id
@@ -67,7 +60,6 @@ class BaseModel():
         """
         save update_at anytime this function is call
         """
-
         self.updated_at = datetime.now()
         models.storage.save()
 
@@ -76,14 +68,11 @@ class BaseModel():
         Return a dictionary containing key and value of the instance\
                 including class name
         """
-
         dic = {}
         for key, value in self.__dict__.items():
             dic[key] = value
             if key in ["created_at", "updated_at"]:
                 dic['created_at'] = self.created_at.isoformat()
                 dic['updated_at'] = self.updated_at.isoformat()
-
         dic['__class__'] = self.__class__.__name__
-
         return dic

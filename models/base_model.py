7f056439bd28dfc28feafd5dbffc0852a070ed27
Beloved-vince <vinceoludare@gmail.com>
2022-11-26 14:22:13 +0100

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
-from uuid import uuid4
 from datetime import datetime
+from uuid import uuid4
 import models
-"""
-BaseModel class is created to generate a dictionary representation\
-        of an instance
-On this class other classes will inherit from
-"""
 
-format_dt = "%Y-%m-%dT%H:%M:%S.%f"
+"""
+Module BaseModel
+Parent of all classes
+"""
 
 
 class BaseModel():
+    """Base class for Airbnb clone project
+    Methods:
+        __init__(self, *args, **kwargs)
+        __str__(self)
+        __save(self)
+        __repr__(self)
+        to_dict(self)
+    """
 
     def __init__(self, *args, **kwargs):
-        """Args:
-                *args: Constructor of BaseModel set to "None"
-                **kwargs: Constructor of BaseModel
-        if **kwargs is not empty operation can be perform
         """
-        if args:
-            pass
-        if kwargs is not None:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_dt)
-                if key not in ['__class__']:
-                    setattr(self, key, item)
+        Initialize attributes: random uuid, dates created/updated
+        """
+        if kwargs:
+            for key, val in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
+                else:
+                    setattr(self, key, val)
+        else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -34,43 +43,33 @@ class BaseModel():
 
     def __str__(self):
         """
-        Return the following objects
-        Args:
-            class name:
-                in format [BaseModel]
-            id:
-                to hold the unique class identifier
-            created_at:
-                        date object is created
-            updated_at:
-                        date object is updated
+        Return string of info about model
         """
-        class_name = self.__class__.__name__
-        uuid = self.id
-        class_dict = self.__dict__
-        return '[{}] ({}) {}'.format(class_name, uuid, class_dict)
+        return ('[{}] ({}) {}'.
+                format(self.__class__.__name__, self.id, self.__dict__))
+
+    def __repr__(self):
+        """
+        returns string representation
+        """
+        return (self.__str__())
 
     def save(self):
         """
-        save update_at anytime this function is call
+        Update instance with updated time & save to serialized file
         """
-
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
-        Return a dictionary containing key and value of the instance\
-                including class name
+        Return dic with string formats of times; add class info to dic
         """
-
         dic = {}
-        for key, value in self.__dict__.items():
-            dic[key] = value
-            if key in ["created_at", "updated_at"]:
-                dic['created_at'] = self.created_at.isoformat()
-                dic['updated_at'] = self.updated_at.isoformat()
-
-        dic['__class__'] = self.__class__.__name__
-
+        dic["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, (datetime, )):
+                dic[k] = v.isoformat()
+            else:
+                dic[k] = v
         return dic
